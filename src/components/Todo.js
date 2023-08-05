aecd34a6e966f058bacf5c8b8e8c813eec14e5e0
AbhishekBhure <abhiab7411@gmail.com>
2023-08-05 17:16:28 +0530

    changes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+
 import edit from "../svg/edit.svg";
 import del from "../svg/del.svg";
 
