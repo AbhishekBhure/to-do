59d5dee072ee0387affc7754c4ca61085b64b196
AbhishekBhure <abhiab7411@gmail.com>
2023-08-14 21:30:46 +0530

    added dark theme properly and changed the bg's to view dark theme properly

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ import todo from "../svg/to-do.svg";
 
 const Navbar = () => {
   return (
-    <nav className="shadow-xl w-full flex justify-between px-7 py-5 bg-[#8758ff] dark:bg-black">
+    <nav className="shadow-xl w-full flex justify-between px-7 py-5">
       <div className="flex justify-center items-center gap-2">
         <img src={todo} alt="todo" className="w-[30px] h-[30px]" />
-        <h1>Agenda!!</h1>
+        <h1 className="text-black font-bold dark:text-white">Agenda!!</h1>
       </div>
       <div className="flex items-center justify-center gap-4">
         <button
