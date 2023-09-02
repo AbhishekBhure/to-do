f8ad8338f828e53f9f0496a86d12094888c47546
AbhishekBhure <abhiab7411@gmail.com>
2023-09-02 22:15:17 +0530

    modified Navbar.js added Link to login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,24 @@
 import React from "react";
 import Switcher from "../Switcher";
 import todo from "../svg/to-do.svg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="shadow-xl w-full flex justify-between px-7 py-5 dark:bg-black fixed">
+    <nav className="shadow-xl w-full flex justify-between px-7 py-5 dark:bg-black fixed z-10">
       <div className="flex justify-center items-center gap-2">
         <img src={todo} alt="todo" className="w-[30px] h-[30px]" />
-        <h1 className="text-black font-bold dark:text-white">Agenda!!</h1>
+        <h1 className="text-black font-bold dark:text-white">
+          {" "}
+          <Link to="/">Agenda!!</Link>{" "}
+        </h1>
       </div>
       <div className="flex items-center justify-center gap-4">
         <button
           type="button"
           className="rounded  dark:text-black transition-all ease-linear  px-5 font-bold py-2  bg-[#ffe100] text-black tracking-normal"
         >
-          <a href="/">Login</a>
+          <Link to="/login">Login</Link>
         </button>
         <Switcher />
       </div>
