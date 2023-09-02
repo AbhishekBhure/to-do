d3446446dbe4ed8e84742471558e128ab70368cc
AbhishekBhure <abhiab7411@gmail.com>
2023-09-02 22:13:03 +0530

    modified App.js added react-router-dom to the app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-
 import Navbar from "./components/Navbar";
 import TodoWrapper from "./components/TodoWrapper";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <TodoWrapper />
+      <Routes>
+        <Route path="/" element={<TodoWrapper />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
     </div>
   );
 }
