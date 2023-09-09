6277da8bd40abf4ea891cfe6f3178586da69b897
AbhishekBhure <abhiab7411@gmail.com>
2023-09-10 00:15:37 +0530

    changed logic in Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,8 +3,7 @@ import { loginFields } from "../contants/formFields";
 import Input from "./Input";
 import FormAction from "./FormAction";
 import FormExtra from "./FormExtra";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
+import { useAuth } from "./Context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
 const fields = loginFields;
@@ -14,6 +13,9 @@ fields.forEach((field) => (fieldsState[field.id] = ""));
 export default function Login() {
   const [loginState, setLoginState] = useState(fieldsState);
   const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const { login, googleSignIn } = useAuth();
 
   const handleChange = (e) => {
     setLoginState({ ...loginState, [e.target.id]: e.target.value });
@@ -25,19 +27,37 @@ export default function Login() {
     navigate("/");
   };
 
-  const authenticateuser = () => {
+  const authenticateuser = async () => {
     let userInput = {};
     fields.forEach((field) => {
       userInput[field.id] = loginState[field.id];
     });
-    signInWithEmailAndPassword(auth, userInput.email, userInput.password)
-      .then((userCredentials) => console.log(userCredentials))
-      .catch((error) => console.log(error));
-    console.log(userInput);
+
+    try {
+      await login(userInput.email, userInput.password);
+    } catch (error) {
+      setError(error.message);
+    }
+
+    // signInWithEmailAndPassword(auth, userInput.email, userInput.password)
+    //   .then((userCredentials) => console.log(userCredentials))
+    //   .catch((error) => console.log(error));
+    // console.log(userInput);
+  };
+
+  const handleGoogleSignIn = async (e) => {
+    e.preventDefault();
+    try {
+      await googleSignIn();
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+    }
   };
 
   return (
     <div className="flex justify-center w-full h-[100vh] bg-white dark:bg-black">
+      {error && <p>{error}</p>}
       <form className="mt-8 space-y-6 " onSubmit={handleSubmit}>
         <div className="-space-y-px">
           {fields.map((field) => (
@@ -58,6 +78,13 @@ export default function Login() {
 
         <FormExtra />
         <FormAction handleSubmit={handleSubmit} text="Login" />
+        <hr />
+        <button
+          onClick={handleGoogleSignIn}
+          className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm  rounded-md text-black bg-[#ffe100] font-bold  transition-all ease-in-out tracking-normal hover:bg-[#e6d343] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#ffe100] mt-10"
+        >
+          Google
+        </button>
       </form>
     </div>
   );
