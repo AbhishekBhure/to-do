769a5a5e3ccd2234beb7ef528ba6cdbd305f1fea
AbhishekBhure <abhiab7411@gmail.com>
2023-09-10 00:13:11 +0530

    added a extra landing page as Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,15 @@ const Home = () => {
   return (
     <>
       <div className="w-full h-[100vh] flex justify-center items-center">
-        <button
-          type="button"
-          className="rounded bg-[#ffe100]  px-5 font-bold py-2 tracking-normal
+        <Link to="/todo">
+          <button
+            type="button"
+            className="rounded bg-[#ffe100]  px-5 font-bold py-2 tracking-normal
       "
-        >
-          <Link to="/todo">Let's Go</Link>
-        </button>
+          >
+            Let's Go
+          </button>
+        </Link>
       </div>
     </>
   );
